5169091d725abe1ee251b6c0f42572a77924f5cb
Umer1299 <90533398+Umer1299@users.noreply.github.com>
2026-03-15 22:08:44 +0500

    Update chatbot-widget.js

--- chatbot-widget.js
+++ chatbot-widget.js
@@ -17,7 +17,7 @@
 
     if (!botId) return;
 
-    var BASE_URL = "https://chatflowai.io/version-test/api/1.1/wf/";
+    var BASE_URL = "https://chatflowai.io/api/1.1/wf/";
     var CONFIG_URL = BASE_URL + "get-chatbot?chatID=" + botId;
     var MESSAGE_URL = BASE_URL + "create-chat";
     var FEEDBACK_URL = "https://chatflowai.io/api/1.1/wf/create-feedback";
